bcca3b8a7035fc111e406de0380ac30465a4bf1c
Graham Wells <graham.wells@gmail.com>
2016-09-06 17:37:50 -0400

    Update availableSoftwareUpdates.sh
    Added link for query

--- availableSoftwareUpdates.sh
+++ availableSoftwareUpdates.sh
@@ -4,7 +4,7 @@
 # Returns "N/A" if the plist doesn't exist.
 #
 # This is designed to be coupled with a policy that runs once per day that specifically queries softwareupdate to see how many 
-# updates are available.
+# updates are available. Find that here: https://github.com/grahamfw/casper/blob/master/queryAppleSUS.sh
 
 # Edit this file path where you will keep a running list of available software updates.
 pList="/var/sandbox/com.company.softwareupdates.plist"
